70b6b507ec220badfa2c164e248fe27606cdecf6
Jdepetr1 P <49722708+jdepetr1@users.noreply.github.com>
2022-04-25 04:21:36 +0000

    Docstrings

--- Rectangle.py
+++ Rectangle.py
@@ -1,5 +1,15 @@
 class Rectangle:
   def __init__(self, x, y, h, w):
+    '''
+      Initializes rectangle object
+    
+      x (int): x value of rectangle (positive)
+      y (int): y value of rectangle (positive)
+      height (int): height of rectangle (positive)
+      width (int): width of rectangle (positive)
+
+      return: None
+    '''
     self.x = x
     self.y = y
     self.height = h
@@ -18,6 +28,11 @@ class Rectangle:
       self.width = 0
       
   def __str__(self):
+    '''
+      Returns string representation of rectangle object
+
+      return: (string)
+    '''
     return f"(x: {self.x}, y: {self.y}) height: {self.height}, width: {self.width}"
 
   
--- Surface.py
+++ Surface.py
@@ -2,8 +2,24 @@ import Rectangle
 
 class Surface:
   def __init__(self, filename, x, y, h, w):
+    '''
+      initializes Surface object by utilizing Rectangle object
+
+      filename (string): name of file
+      x (int): x value of rectangle (positive)
+      y (int): y value of rectangle (positive)
+      height (int): height of rectangle (positive)
+      width (int): width of rectangle (positive)
+
+      return: None
+    '''
     self.rect = Rectangle.Rectangle(x, y, h, w)
     self.image = filename
 
   def getRect(self):
+    '''
+      Returns Rectangle object of Surface object
+
+      return: (Rectangle)
+    '''
     return self.rect
